d64fa89b52e7ea8753eb5355c1d82988d703f0c1
Chen Huang <p13761495164@gmail.com>
2026-07-14 14:39:13 +0900

    Add staff clearance below playback controls

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 "use strict";
 
-const CACHE_NAME = "easy-piano-v144";
+const CACHE_NAME = "easy-piano-v145";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=144",
-  "./app.js?v=144",
+  "./styles.css?v=145",
+  "./app.js?v=145",
   "./manifest.webmanifest",
-  "./icons/icon.png?v=144",
-  "./icons/icon-192.png?v=144",
-  "./icons/icon-512.png?v=144"
+  "./icons/icon.png?v=145",
+  "./icons/icon-192.png?v=145",
+  "./icons/icon-512.png?v=145"
 ];
 
 self.addEventListener("install", (event) => {
