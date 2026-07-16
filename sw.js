3a6cb61d877fc26562e01c961fd0d24f4f13ebd5
Chen Huang <p13761495164@gmail.com>
2026-07-17 05:13:45 +0900

    Revert "Use time sections for tolerance follow"
    This reverts commit 88f93208dc797fddc5a7e538a1680a099d1bfff3.

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 "use strict";
 
-const CACHE_NAME = "easy-piano-v208";
+const CACHE_NAME = "easy-piano-v207";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=208",
-  "./app.js?v=208",
+  "./styles.css?v=207",
+  "./app.js?v=207",
   "./manifest.webmanifest",
-  "./icons/icon.png?v=208",
-  "./icons/icon-192.png?v=208",
-  "./icons/icon-512.png?v=208"
+  "./icons/icon.png?v=207",
+  "./icons/icon-192.png?v=207",
+  "./icons/icon-512.png?v=207"
 ];
 
 self.addEventListener("install", (event) => {
