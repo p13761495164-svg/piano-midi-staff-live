66528488b860c3fec5455a973422318391fee07b
Chen Huang <p13761495164@gmail.com>
2026-07-17 05:13:45 +0900

    Revert "Mark tolerated sections as processed"
    This reverts commit e54f06dd13204ed40da73427fb9ce02424671b2c.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const APP_VERSION = "v210";
+const APP_VERSION = "v209";
 const MIDI_MIN = 21;
 const MIDI_MAX = 108;
 const DEFAULT_WHITE_KEY_WIDTH_PX = 38;
@@ -2848,7 +2848,6 @@ function evaluateSectionAutoFollow(options = {}) {
     return;
   }
   if (!isTargetGroupMatched(currentGroup)) return;
-  markTargetGroupProcessed(currentGroup);
 
   const groupEndTick = Math.max(...currentGroup.map((target) => target.startTick));
   const nextGroup = nextUnmatchedTargetGroupAfterTick(groupEndTick + 1);
@@ -2856,14 +2855,6 @@ function evaluateSectionAutoFollow(options = {}) {
     ? Math.min(...nextGroup.map((target) => target.startTick))
     : practiceEndTick();
   animatePracticeViewToTick(nextTick, { snap: false });
-}
-
-function markTargetGroupProcessed(targets) {
-  targets.forEach((target) => {
-    if (!isAutoFollowTargetMatched(target)) {
-      markPlayedTargetForBeat(beatStartForTick(target.startTick), target);
-    }
-  });
 }
 
 function isAutoFollowTargetMatched(target) {
--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 "use strict";
 
-const CACHE_NAME = "easy-piano-v210";
+const CACHE_NAME = "easy-piano-v209";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=210",
-  "./app.js?v=210",
+  "./styles.css?v=209",
+  "./app.js?v=209",
   "./manifest.webmanifest",
-  "./icons/icon.png?v=210",
-  "./icons/icon-192.png?v=210",
-  "./icons/icon-512.png?v=210"
+  "./icons/icon.png?v=209",
+  "./icons/icon-192.png?v=209",
+  "./icons/icon-512.png?v=209"
 ];
 
 self.addEventListener("install", (event) => {
